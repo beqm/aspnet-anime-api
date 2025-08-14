fdc0627924e7db394bcf28bf2fd95686537c6385
beqm <brenomartinsmb@gmail.com>
2025-08-14 17:23:50 -0300

    feat: post and get routes

---
+++ Api/Controllers/AnimeController.cs
@@ -0,0 +1,43 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+using Application.Commands.Anime.CreateAnime;
+using Application.Queries.Anime.GetAnimeById;
+using Application.Queries.Anime.GetAnimeRange;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/v1/anime")]
+public class AnimeController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public AnimeController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateAnimeCommand command)
+    {
+        var result = await _mediator.Send(command);
+        return CreatedAtAction(nameof(GetById), new { id = result.ID }, result);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var anime = await _mediator.Send(new GetAnimeByIdQuery(id));
+        if (anime == null) return NotFound();
+        return Ok(anime);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetRange([FromQuery] int start = 1, [FromQuery] int end = 10)
+    {
+        var query = new GetAnimeRangeQuery(start, end);
+        var result = await _mediator.Send(query);
+        return Ok(result);
+    }
+}
